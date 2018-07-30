1ec35e06dbfeaaf7e0a64996b0298d95b95b27d3
Gary Muir <garymuir17@gmail.com>
2018-07-30 15:25:51 +0100

    solves episode one

---
+++ whoDunnit.js
@@ -0,0 +1,16 @@
+// EPISODE 1
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Miss Scarlet. All constant variables are declared at the same level and so can see each other.
